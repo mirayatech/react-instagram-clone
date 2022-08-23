9eef959063e1c73c5616dba90c3d366a4ebd513c
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-08-23 18:19:29 +0200

    feat: slide to left and right button

--- src/components/Feed/Story/Stories.tsx
+++ src/components/Feed/Story/Stories.tsx
@@ -1,11 +1,23 @@
+import { useRef } from 'react'
 import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io'
 import { Profiles } from '../../../data/profile'
 import { Story } from './Story'
 import '/src/styles/stories.css'
 export function Stories() {
+  const sliderRef = useRef<any>(null)
+
+  const slideLeft = () => {
+    let slider = sliderRef.current
+    slider.scrollLeft = slider.scrollLeft - 100
+  }
+  const slideRight = () => {
+    let slider = sliderRef.current
+    slider.scrollLeft = slider.scrollLeft + 100
+  }
+
   return (
-    <div className="stories">
-      <button className="scroll__btn left">
+    <div className="stories" ref={sliderRef}>
+      <button onClick={slideLeft} className="scroll__btn left">
         <IoIosArrowBack />
       </button>
       {Profiles.map((profile, id) => (
@@ -13,7 +25,7 @@ export function Stories() {
           <Story profile={profile} key={id} />
         </div>
       ))}
-      <button className="scroll__btn left">
+      <button onClick={slideRight} className="scroll__btn right">
         <IoIosArrowForward />{' '}
       </button>
     </div>
