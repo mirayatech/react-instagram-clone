6f9291769887d6451edcc91019358a0d23c4c635
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-08-30 18:50:59 +0200

    feat: able to dign in with google

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,7 +1,15 @@
 import '../../styles/Login.css'
 import { GrGoogle } from 'react-icons/gr'
+import { signInWithPopup } from 'firebase/auth'
+import { firebaseAuth, googleAuthProvider } from '../../firebase/firebase'
 import AnimePicture from '../../images/anime.png'
+import { useNavigate } from 'react-router-dom'
+
 export function Login() {
+  const navigate = useNavigate()
+  const signInWithGoogle = () => {
+    signInWithPopup(firebaseAuth, googleAuthProvider).then(() => navigate('/'))
+  }
   return (
     <div className="Login">
       <img src={AnimePicture} alt="Two phones" className="login__picture" />
@@ -18,7 +26,7 @@ export function Login() {
           posts.
         </p>
 
-        <button>
+        <button onClick={signInWithGoogle}>
           <GrGoogle className="svg" /> Sign in with Google
         </button>
       </div>
