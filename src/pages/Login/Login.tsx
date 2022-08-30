2e3fb6cd962d517a1a87cd2c544c820225f51938
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-08-30 17:48:38 +0200

    feat: creating login layout / component

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,3 +1,31 @@
+import '../../styles/Login.css'
+import { GrGoogle } from 'react-icons/gr'
+
 export function Login() {
-  return <div>Login</div>
+  return (
+    <div className="Login">
+      <img
+        src="https://betelgeusetech.com/wp-content/uploads/2021/05/instadevice.png"
+        alt="Two phones"
+        className="login__picture"
+      />
+
+      <div className="login__form">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1280px-Instagram_logo.svg.png"
+          alt="Instagram logo"
+        />
+
+        <p>
+          Sign in with Google to see what your favorite anime characters have
+          posted. Like their posts, comment on their posts and upload your own
+          posts.
+        </p>
+
+        <button>
+          <GrGoogle className="svg" /> Sign in with Google
+        </button>
+      </div>
+    </div>
+  )
 }
