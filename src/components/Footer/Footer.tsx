565a0df6b27775516048e3740cfa478245906d0a
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-08-31 08:33:00 +0200

    feat: create footer layout and style it

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import '../../styles/Footer.css'
+
+export function Footer() {
+  return (
+    <footer>
+      Built with <span className="footer__heart">💙</span> by{' '}
+      <a href="https://github.com/mirayatech" target="_blank">
+        Miraya
+      </a>
+    </footer>
+  )
+}
