ac5950f32e0f84e6e563c8c5ad16c20ea12040a2
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-08-31 08:34:10 +0200

    feat: import footer to feed

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,5 +1,6 @@
 import '../../styles/Login.css'
 import { GrGoogle } from 'react-icons/gr'
+import { Footer } from '../../exportFiles'
 import { signInWithPopup } from 'firebase/auth'
 import { firebaseAuth, googleAuthProvider } from '../../firebase/firebase'
 import AnimePicture from '../../images/anime.png'
@@ -11,25 +12,28 @@ export function Login() {
     signInWithPopup(firebaseAuth, googleAuthProvider).then(() => navigate('/'))
   }
   return (
-    <div className="Login">
-      <img src={AnimePicture} alt="Two phones" className="login__picture" />
+    <>
+      <div className="Login">
+        <img src={AnimePicture} alt="Two phones" className="login__picture" />
 
-      <div className="login__form">
-        <img
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1280px-Instagram_logo.svg.png"
-          alt="Instagram logo"
-        />
+        <div className="login__form">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1280px-Instagram_logo.svg.png"
+            alt="Instagram logo"
+          />
 
-        <p>
-          Sign in with Google to see what your favorite anime characters have
-          posted. Like their posts, comment on their posts and upload your own
-          posts.
-        </p>
+          <p>
+            Sign in with Google to see what your favorite anime characters have
+            posted. Like their posts, comment on their posts and upload your own
+            posts.
+          </p>
 
-        <button onClick={signInWithGoogle}>
-          <GrGoogle className="svg" /> Sign in with Google
-        </button>
+          <button onClick={signInWithGoogle}>
+            <GrGoogle className="svg" /> Sign in with Google
+          </button>
+        </div>
       </div>
-    </div>
+      <Footer />
+    </>
   )
 }
