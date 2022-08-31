87aa1e04f03ef5b542d70638619826b436f30665
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-08-31 18:34:09 +0200

    refactor: export as named function

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,8 @@ import { Feed, Login } from './exportFiles'
 import { Routes, Route } from 'react-router-dom'
 import { UserAuthContextProvider } from './context/context'
 import { ProtectedRoute } from './components/ProtectedRoute'
-import { onAuthStateChanged } from 'firebase/auth'
-import { useState, useEffect } from 'react'
-import { firebaseAuth } from './library/firebase'
 
-export default function App() {
-  const [isUserSignedIn, setIsUserSignedIn] = useState(true)
-
+export function App(): JSX.Element {
   return (
     <UserAuthContextProvider>
       <Routes>
