70fb2bc837c0c779e97d3b57b5dc36ec342c1aa5
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-08-31 08:36:14 +0200

    export footer by name

--- src/exportFiles.tsx
+++ src/exportFiles.tsx
@@ -1,3 +1,4 @@
-export { Header } from './Header/Header'
+export { Header } from './components/Header/Header'
 export { Feed } from './pages/Feed/Feed'
 export { Login } from './pages/Login/Login'
+export { Footer } from './components/Footer/Footer'
