151562854b8bd09053ea3088d5b61a1b412c1ecb
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-09-04 12:33:37 +0200

    create newdata for side profiles

---
+++ src/library/SideProfiles.ts
@@ -0,0 +1,31 @@
+export const SideProfiles = [
+  {
+    picture: 'https://avatarfiles.alphacoders.com/235/thumb-235828.png',
+    username: 'mitsuki__official',
+    suggestions: 'Followed by uzumaki_boruto',
+  },
+
+  {
+    picture:
+      'https://bestprofilepictures.com/wp-content/uploads/2021/06/Zenitsu-Profile-Pic.jpg',
+    username: 'zenitsu',
+    suggestions: 'Followed by tanjiro + 1 more',
+  },
+  {
+    picture:
+      'https://i.pinimg.com/736x/59/b3/56/59b3569f24106c678148d1ecaec16f08.jpg',
+    username: 'levi_ackerman',
+    suggestions: 'Follows you',
+  },
+  {
+    picture:
+      'https://i.pinimg.com/originals/4d/6c/a5/4d6ca51b0ff1524f1f791ef3f646a921.jpg',
+    username: 'suzune_horikita',
+    suggestions: 'New to Instagram',
+  },
+  {
+    picture: 'https://avatarfiles.alphacoders.com/178/178573.jpg',
+    username: 'sasuke_uchiha',
+    suggestions: 'Followed by tanjiro + 5 more',
+  },
+]
