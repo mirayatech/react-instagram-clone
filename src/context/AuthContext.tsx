b17ff948ed578909578e7ab4d6865bd622381235
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-09-12 14:41:01 +0200

    rebuild auth context component

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,43 @@
+import { useContext, createContext, useEffect, useState } from 'react'
+import {
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+  onAuthStateChanged,
+} from 'firebase/auth'
+import { firebaseAuth } from '../library/firebase'
+
+const AuthContext = createContext()
+
+export const AuthContextProvider = ({ children }) => {
+  const [user, setUser] = useState({})
+
+  const googleSignIn = () => {
+    const provider = new GoogleAuthProvider()
+    signInWithPopup(firebaseAuth, provider)
+  }
+
+  const logOut = () => {
+    signOut(firebaseAuth)
+  }
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(firebaseAuth, (currentUser) => {
+      setUser(currentUser)
+      console.log('User', currentUser)
+    })
+    return () => {
+      unsubscribe()
+    }
+  }, [])
+
+  return (
+    <AuthContext.Provider value={{ googleSignIn, logOut, user }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export const UserAuth = () => {
+  return useContext(AuthContext)
+}
